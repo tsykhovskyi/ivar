f9de863805586d26c46da2382a4fd281fcf7782e
Ihor Tsykhovskyi <igorppok@gmail.com>
2022-09-06 21:18:10 +0300

    timeout for commander response

--- src/ldb/commander.ts
+++ src/ldb/commander.ts
@@ -1,14 +1,36 @@
 import { ChildProcess, spawn } from "child_process";
 import EventEmitter from "events";
 
+interface Logger {
+  log(data: any): void;
+}
+
+class ConsoleLogger {
+  log(data: any) {
+    if (process.env.DEBUG === 'true') {
+      console.dir(data);
+    }
+  }
+}
+
+const DEFAULT_RESPONSE_HANDLER_DELAY_MS = 7;
+
 export class Commander extends EventEmitter {
   private cmd: ChildProcess | null = null;
+
+  private lastMS = 0;
+  private requestBuffer: string[] = [];
 
   private responseResolver: Function | null = null;
   private responseRejector: Function | null = null;
   private buffer: string = '';
+  private responseHandleTimer: NodeJS.Timeout | null = null;
 
-  constructor(private args: string[]) {
+  constructor(
+    private args: string[],
+    private responseHandlerDelayMs: number = DEFAULT_RESPONSE_HANDLER_DELAY_MS,
+    private logger: Logger = new ConsoleLogger()
+  ) {
     super()
   }
 
@@ -33,6 +55,7 @@ export class Commander extends EventEmitter {
       throw new Error('command is not accessible')
     }
 
+    this.requestBuffer.push(msg);
     cmd.stdin?.write(msg + "\n");
 
     return this.makeResponse();
@@ -47,18 +70,21 @@ export class Commander extends EventEmitter {
 
   private onStdout(chunk: Uint8Array) {
     this.buffer += chunk.toString();
-    const endOfResponse = chunk[chunk.length - 1] === 10; // it is still possible to receive partial with last symbol \n
+    this.logChunk(chunk);
 
-    if (this.responseResolver) {
-      if (endOfResponse) {
-        this.responseResolver(this.buffer);
-        this.responseResolver = null;
-        this.responseRejector = null;
-        this.buffer = '';
-      }
-    } else {
-      this.buffer = '';
+    // clear current response timer if exists
+    if (this.responseHandleTimer) {
+      clearTimeout(this.responseHandleTimer)
     }
+    // set timer for response resolve which allows to not miss delayed request chunks.
+    this.responseHandleTimer = setTimeout(() => {
+      if (this.responseResolver) {
+        this.responseResolver(this.buffer);
+      }
+      this.responseResolver = null;
+      this.responseRejector = null;
+      this.buffer = '';
+    }, this.responseHandlerDelayMs);
   }
 
   private onStderr(chunk: Uint8Array) {
@@ -72,5 +98,19 @@ export class Commander extends EventEmitter {
 
   close() {
     this.cmd?.stdin?.end();
+  }
+
+  private logChunk(chunk: Uint8Array) {
+    const now = Date.now();
+    const msg = chunk.toString();
+    this.logger.log({
+      requests: this.requestBuffer,
+      deltaMS: now - this.lastMS,
+      chunk: msg,
+      buffer: this.buffer,
+    });
+    this.lastMS = now;
+    this.requestBuffer = []
+
   }
 }
