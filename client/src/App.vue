a2cd6af482817e17c230314b94cb3e1ea99192eb
Ihor Tsykhovskyi <igorppok@gmail.com>
2022-12-27 23:20:06 +0200

    ui decouple pages components

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,67 +1,19 @@
 <script setup lang="ts">
-import Toolbar from './components/Toolbar.vue';
-import Debugger from './components/Debugger.vue';
-import Tabs from './components/Tabs.vue';
-import { api } from '@/api';
-import { onMounted, ref } from 'vue';
-import type { DebuggerResponse, Session } from '@/api/api';
-import { DebuggerState } from '@/api/api';
+import DebugPage from './components/debug.page.vue';
+import { ref } from 'vue';
 
-const sessions = ref<Session[]>([]);
-const activeSession = ref<string | null>(null);
-const debuggerResponse = ref<DebuggerResponse | null>(null);
-
-async function toggleActiveSession(sessionIdSelected: string) {
-  activeSession.value =
-    activeSession.value !== sessionIdSelected ? sessionIdSelected : null;
-  api.setSessionId(activeSession.value);
-
-  if (activeSession.value !== null) {
-    debuggerResponse.value = await api.refresh();
-  } else {
-    debuggerResponse.value = null;
-  }
-}
-
-async function closeSession(sessionId: string) {
-  await api.finishSession(sessionId);
-}
-
-onMounted(async () => {
-  const updateSessions = (_sessions: Session[]) => {
-    sessions.value = [..._sessions].sort(
-      (a, b) => b.time.started - a.time.started
-    );
-    if (_sessions.length > 0) {
-      const lastRunningSession = [..._sessions]
-        .filter((s) => s.state === DebuggerState.Running)
-        .sort((a, b) => b.time.updated - a.time.updated)[0];
-      if (activeSession.value !== lastRunningSession.id) {
-        toggleActiveSession(lastRunningSession.id);
-      }
-    } else {
-      activeSession.value = null;
-    }
-  };
-  api.onSessionsUpdate(updateSessions);
-  api.onDebuggerResponse((response) => (debuggerResponse.value = response));
-  updateSessions(await api.sessions());
-});
+let page = ref<string>('debugger');
 </script>
 
 <template>
-  <div class="container is-fluid" style="height: 100vh">
-    <div style="height: 150px">
-      <Toolbar :is-active="!!activeSession" />
-      <Tabs
-        :sessions="sessions"
-        :active-session="activeSession"
-        @onSessionToggle="toggleActiveSession($event)"
-        @onSessionClose="closeSession($event)"
-      ></Tabs>
+  <nav class="navbar is-transparent">
+    <div class="navbar-menu">
+      <div class="navbar-start">
+        <a class="navbar-item" :class="{'is-active':page==='debugger'}" @click="page='debugger'">Debugger</a>
+        <a class="navbar-item" :class="{'is-active':page==='logger'}" @click="page='logger'">Logger</a>
+      </div>
     </div>
-    <div style="height: calc(100vh - 150px)">
-      <Debugger :debugger-response="debuggerResponse" />
-    </div>
-  </div>
+  </nav>
+
+  <DebugPage v-if="page==='debugger'"></DebugPage>
 </template>
---
+++ client/src/components/debug.page.vue
@@ -0,0 +1,64 @@
+<script setup lang="ts">
+import Toolbar from './../components/debugger/Toolbar.vue';
+import Debugger from './../components/debugger/Debugger.vue';
+import Tabs from './../components/debugger/Tabs.vue';
+import { api } from '@/api';
+import { onMounted, ref } from 'vue';
+import type { DebuggerResponse, Session } from '@/api/api';
+
+const sessions = ref<Session[]>([]);
+const activeSession = ref<string | null>(null);
+const debuggerResponse = ref<DebuggerResponse | null>(null);
+
+async function toggleActiveSession(sessionIdSelected: string) {
+  activeSession.value =
+      activeSession.value !== sessionIdSelected ? sessionIdSelected : null;
+  api.setSessionId(activeSession.value);
+
+  if (activeSession.value !== null) {
+    debuggerResponse.value = await api.refresh();
+  } else {
+    debuggerResponse.value = null;
+  }
+}
+
+async function closeSession(sessionId: string) {
+  await api.finishSession(sessionId);
+}
+
+onMounted(async () => {
+  const updateSessions = (_sessions: Session[]) => {
+    sessions.value = [..._sessions].sort(
+        (a, b) => b.time.updated - a.time.updated
+    );
+    if (_sessions.length > 0) {
+      const lastRunningSession = sessions.value[0];
+      if (activeSession.value !== lastRunningSession.id) {
+        toggleActiveSession(lastRunningSession.id);
+      }
+    } else {
+      activeSession.value = null;
+    }
+  };
+  api.onSessionsUpdate(updateSessions);
+  api.onDebuggerResponse((response) => (debuggerResponse.value = response));
+  updateSessions(await api.sessions());
+});
+</script>
+
+<template>
+  <div class="container is-fluid" style="height: calc(100vh - 60px)">
+    <div style="height: 150px">
+      <Toolbar :is-active="!!activeSession"/>
+      <Tabs
+          :sessions="sessions"
+          :active-session="activeSession"
+          @onSessionToggle="toggleActiveSession($event)"
+          @onSessionClose="closeSession($event)"
+      ></Tabs>
+    </div>
+    <div style="height: calc(100vh - 60px - 150px)">
+      <Debugger :debugger-response="debuggerResponse"/>
+    </div>
+  </div>
+</template>
